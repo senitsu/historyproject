2538e8ee184688cf35757d773a83bb7e1a7b53b1
senitsu <89802983+senitsu@users.noreply.github.com>
2022-02-08 11:14:04 -0800

    added stuff

---
+++ js/script.js
@@ -0,0 +1,105 @@
+/* global monogatari */
+
+// Define the messages used in the game.
+monogatari.action ('message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('notification').notifications ({
+	'Welcome': {
+		title: 'Welcome',
+		body: 'This is the Monogatari VN Engine',
+		icon: ''
+	}
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('particles').particles ({
+
+});
+
+// Define the canvas objects used in the game
+monogatari.action ('canvas').objects ({
+
+});
+
+// Credits of the people involved in the creation of this awesome game
+monogatari.configuration ('credits', {
+
+});
+
+
+// Define the images that will be available on your game's image gallery
+monogatari.assets ('gallery', {
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voices', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sounds', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('videos', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+
+});
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+
+});
+
+
+// Define the Characters
+monogatari.characters ({
+
+});
+
+//
+monogatari.script ({
+	'Start': [
+
+	]
+});
+
+monogatari.component ('main-screen').template (() => {
+    return `
+				<br/>
+				<br/>
+				<br/>
+        <h1>Put Your Title Here</h1>
+        <p>
+        By Senya W.
+        </p>
+				<br/>
+        <center>
+				<img src="assets/images/jettPX.gif">
+				<img src="assets/images/phoenixPX.gif">
+        </center>
+
+        <main-menu></main-menu>
+    `;
+});
